a81dd855f02595d4d7161630144d8fef6d79fc64
minhajul islam <mm01882390860@gmail.com>
2026-03-22 17:39:13 +0600

    style show all button with gradient

--- src/components/TopApps/TopApp.jsx
+++ src/components/TopApps/TopApp.jsx
@@ -64,9 +64,9 @@ const TopApp = () => {
 
       
       
-        <div>
+        <div className='flex justify-center'>
 
-            <button onClick={() => navigate('/all-apps')} className='btn btn-primary flex items-center mt-6'>Show All</button>
+            <button onClick={() => navigate('/all-apps')} className='btn btn-primary rounded-xl bg-gradient-to-r from-[#632EE3] to-[#9F62F2] flex items-center mt-6 text-white border-none'>Show All</button>
         </div>
 
         </>
